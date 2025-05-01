7eaf5fa3e24525445a461fbc16871d9cd6358111
Amany Gaber <116579137+amany-gaber@users.noreply.github.com>
2025-05-01 16:56:42 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,3 +1,47 @@
+# main .py
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from routes import router
+import uvicorn
+from config import settings
+from schemas import ExceptionHandler
+
+app = FastAPI()
+
+# Enable CORSd
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the routes from routes.py
+app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"message": "Click /docs to see the API documentation"}
+
+@app.exception_handler(404)
+def not_found_error(request, exc):
+    return {"detail": "Page Not Found. Click /docs to see the API documentation"}
+
+@app.exception_handler(Exception)
+def handle_exception(request, exc):
+    return {"message": f"Oops! {str(exc)}. Please try again!"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.DOMAIN,
+        port=settings.BACKEND_PORT,
+        reload=settings.DEBUG_MODE,
+    )
+
+
+
 # user.py
 from fastapi import APIRouter, File, UploadFile, HTTPException
 import numpy as np
