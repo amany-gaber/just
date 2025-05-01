94018842f80d3407a53a15cf65b354c31d62275c
Amany Gaber <116579137+amany-gaber@users.noreply.github.com>
2025-05-01 16:39:46 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,28 +1,14 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException
-import numpy as np
-from services import EmotionInference
+import uvicorn
+from fastapi import APIRouter
+from services import *
 
-router = APIRouter(
+service = APIRouter(
     prefix="/CV_Specific_Job",
-    tags=["predict"]
+    tags=["Perdict"]
 )
 
-model_inference = EmotionInference()
 
-@router.post("/inference")
-async def inference(file: UploadFile = File(...)):
-    file_extension = file.filename.split(".")[-1].lower()
-    supported_formats = ["wav", "mp3", "ogg", "opus"]
+if __name__ == "__main__":
+    uvicorn.run(service)
 
-    if file_extension not in supported_formats:
-        raise HTTPException(status_code=400, detail="Unsupported file format. Please upload WAV, MP3, OGG, or OPUS.")
-
-    try:
-        audio_bytes = await file.read()
-
-        result = model_inference.run_inference(audio_bytes, file_extension)
-
-        return result
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+# uvicorn main:app --reload
